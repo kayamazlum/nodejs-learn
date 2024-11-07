00f581328c3726de8c4d88ed038f02c18726d76c
kayamazlum <mazlum09kaya@gmail.com>
2024-11-07 22:23:59 +0300

    paketler yuklendi klasorler kuruldu server kuruldu veritabanı baglandı

---
+++ src/db/dbConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Veritabanına bağlandııı");
+  })
+  .catch((err) => {
+    console.log("veritabanına bağlanamadı.!", err);
+  });

2b9e27ed498ce13bd4de239bb5a0eb67e96194f9
kayamazlum <mazlum09kaya@gmail.com>
2024-11-12 23:55:54 +0300

    joi validations yapıldı

---
+++ src/middlewares/validations/auth.validations.js
@@ -0,0 +1,59 @@
+const joi = require("joi");
+const APIError = require("../../utils/errors");
+
+class authValidation {
+  constructor() {}
+  static register = async (req, res, next) => {
+    try {
+      await joi
+        .object({
+          name: joi.string().trim().min(3).max(50).required().messages({
+            "string.base": "İsim alanı normal metin olmalıdır.",
+            "string.empty": "İsim alanı boş olamaz!",
+            "string.min": "İsim alanı en az 3 karakterden oluşmalıdır.",
+            "string.max": "İsim alanı en fazla 50 karakterden oluşabilir.",
+            "string.required": "İsim alanı zorunludur.",
+          }),
+          lastname: joi.string().trim().min(3).max(50).required().messages({
+            "string.base": "Soyad alanı normal metin olmalıdır.",
+            "string.empty": "Soyad alanı boş olamaz!",
+            "string.min": "Soyad alanı en az 3 karakterden oluşmalıdır.",
+            "string.max": "Soyad alanı en fazla 50 karakterden oluşabilir.",
+            "string.required": "Soyad alanı zorunludur.",
+          }),
+          email: joi
+            .string()
+            .email()
+            .trim()
+            .min(3)
+            .max(50)
+            .required()
+            .messages({
+              "string.base": "Email alanı normal metin olmalıdır.",
+              "string.empty": "Email alanı boş olamaz!",
+              "string.min": "Email alanı en az 3 karakterden oluşmalıdır.",
+              "string.email": "Lütfen geçerli bir mail adresi giriniz",
+              "string.max": "Email alanı en fazla 50 karakterden oluşabilir.",
+              "string.required": "Email alanı zorunludur.",
+            }),
+          password: joi.string().trim().min(6).max(36).required().messages({
+            "string.base": "Password alanı normal metin olmalıdır.",
+            "string.empty": "Password alanı boş olamaz!",
+            "string.min": "Password alanı en az 6 karakterden oluşmalıdır.",
+            "string.max": "Password alanı en fazla 36 karakterden oluşabilir.",
+            "string.required": "Password alanı zorunludur.",
+          }),
+        })
+        .validateAsync(req.body);
+    } catch (error) {
+      if (error.details && error?.details[0].message) {
+        throw new APIError(error.detail[0].message, 400);
+      } else {
+        throw new APIError("Lütfen validasyon kurallarına uyun", 400);
+      }
+    }
+    next();
+  };
+}
+
+module.exports = authValidation;
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { login, register } = require("../controller/auth.controller");
+const authValidation = require("../middlewares/validations/auth.validations");
 
 router.post("/login", login);
-router.post("/register", register);
+router.post("/register", authValidation.register, register);
 
 module.exports = router;

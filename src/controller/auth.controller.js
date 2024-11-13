52de7cf45d538f9814cdab36a1b49c179f9da416
kayamazlum <mazlum09kaya@gmail.com>
2024-11-14 00:18:58 +0300

    login validation yapildi, token olusturma yapildi

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -3,10 +3,19 @@ const user = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const APIError = require("../utils/errors");
 const Response = require("../utils/response");
+const { createToken } = require("../middlewares/auth");
 
 const login = async (req, res) => {
-  console.log(req.body);
-  return res.json(req.body);
+  const { email, password } = req.body;
+  const userInfo = await user.findOne({ email });
+  console.log(userInfo);
+  if (!userInfo) throw new APIError("Email ya da şifre hatalıdır.", 401);
+
+  const comparePassword = await bcrypt.compare(password, userInfo.password);
+  console.log(comparePassword);
+  if (!comparePassword) throw new APIError("Email ya da şifre hatalıdır.", 401);
+
+  createToken(userInfo, res);
 };
 const register = async (req, res) => {
   const { email } = req.body;
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { login, register } = require("../controller/auth.controller");
 const authValidation = require("../middlewares/validations/auth.validations");
 
-router.post("/login", login);
+router.post("/login", authValidation.login, login);
 router.post("/register", authValidation.register, register);
 
 module.exports = router;
